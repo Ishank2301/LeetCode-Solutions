816424846b107a9aa78f7fc9ea6b068a5cb21848
Ishank Mishra <ishankmishra579@gmail.com>
2026-06-10 23:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.59%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -3,3 +3,19 @@ class Solution:
         s = s.strip()
         s = s.split()
         return len(s[-1]) 
+
+
+# Another nice way is that use two variables iterate in the string while checking if there is no space update count for letters or word if space reset count in end return the count
+
+"""
+     i = 0
+     for ch in s:
+        
+        if ch!=" ":
+            i+=1
+            j = i
+        else:
+            i = 0 
+        return j
+        
+"""
