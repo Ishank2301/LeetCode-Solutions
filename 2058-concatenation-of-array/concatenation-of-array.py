abf31f795160fb6d0b7acf659773d95f9326b977
Ishank Mishra <ishankmishra579@gmail.com>
2026-06-10 21:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.66%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # we wil need to concatenate two lists to make a better.
+        # integer array nums of length
+        return nums+nums
+
+        # In this we need to append the same array to itself now we will try to append the array
+        
