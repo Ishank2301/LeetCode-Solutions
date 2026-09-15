4c7b98067f0d350815077e695e1ee329b908dbd4
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:37:18 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.py
@@ -0,0 +1,22 @@
+# Last updated: 15/9/2026, 11:37:22 pm
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        s = s.split()
+        return len(s[-1]) 
+
+
+# Another nice way is that use two variables iterate in the string while checking if there is no space update count for letters or word if space reset count in end return the count
+
+"""
+     i = 0
+     for ch in s:
+        
+        if ch!=" ":
+            i+=1
+            j = i
+        else:
+            i = 0 
+        return j
+        
+"""
