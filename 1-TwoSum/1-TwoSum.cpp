392af3c99fa0e81a57f4bf5928de3a4d1d6012b1
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:38:46 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,18 @@
+// Last updated: 15/9/2026, 11:38:50 pm
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            int num = nums[i];
+            int moreNeeded = target - num;
+            if(mpp.find(moreNeeded) != mpp.end()){
+            return{mpp[moreNeeded], i};
+            }
+            mpp[num] = i;
+
+        }
+        return {-1, -1};
+    }
+};
