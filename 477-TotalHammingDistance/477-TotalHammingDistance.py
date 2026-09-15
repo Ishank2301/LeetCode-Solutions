4d11ea86863fac6d09190c97bcd989bb1d154dd0
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:35:55 +0530

    Create 477-TotalHammingDistance

---
+++ 477-TotalHammingDistance/477-TotalHammingDistance.py
@@ -0,0 +1,4 @@
+# Last updated: 15/9/2026, 11:35:59 pm
+class Solution(object):
+    def totalHammingDistance(self, nums):
+        return sum(b.count('1') * b.count('0') for b in zip(*map('{:032b}'.format, nums)))
