69154a8255847608539dbda894287da252926f1b
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:33:08 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ 3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.py
@@ -0,0 +1,8 @@
+# Last updated: 15/9/2026, 11:33:12 pm
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n
