0d634aa1d4f78745cc1690b7b796ffe5a2af1174
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:34:30 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.py
@@ -0,0 +1,21 @@
+# Last updated: 15/9/2026, 11:34:34 pm
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        new = ""
+        i, j = 0, 0
+
+        while i < len(word1) and j < len(word2):
+            new += word1[i]
+            new += word2[j]
+            i += 1
+            j += 1
+
+        while i < len(word1):
+            new += word1[i]
+            i += 1
+
+        while j < len(word2):
+            new += word2[j]
+            j += 1
+
+        return new
