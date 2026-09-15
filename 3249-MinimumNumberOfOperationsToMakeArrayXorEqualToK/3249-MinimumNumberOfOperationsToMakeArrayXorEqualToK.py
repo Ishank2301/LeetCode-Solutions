e588648c89c1f156bf683e3478eb17b25fc813d3
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:33:37 +0530

    Create 3249-MinimumNumberOfOperationsToMakeArrayXorEqualToK

---
+++ 3249-MinimumNumberOfOperationsToMakeArrayXorEqualToK/3249-MinimumNumberOfOperationsToMakeArrayXorEqualToK.py
@@ -0,0 +1,12 @@
+# Last updated: 15/9/2026, 11:33:41 pm
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        
+        current_xor = 0
+        
+        # Step 1: Find the total XOR sum of the array
+        for num in nums:
+            current_xor ^= num
+            
+        # Step 2 & 3: XOR with k to find differing bits, then count them
+        return (current_xor ^ k).bit_count()
