eabea401125363d9ca4db98eda57657f1daa5d40
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:33:02 +0530

    Create 4080-SmallestMissingMultipleOfK

---
+++ 4080-SmallestMissingMultipleOfK/4080-SmallestMissingMultipleOfK.py
@@ -0,0 +1,10 @@
+# Last updated: 15/9/2026, 11:33:06 pm
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        x = 0
+        for n in nums:
+            if not (n % k):
+                i = (n // k) - 1
+                x |= 1 << i
+
+        return ((x + 1) & ~x).bit_length() * k
