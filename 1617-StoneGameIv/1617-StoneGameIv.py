21de0ce688e114fa7f96d1b6b9f5e6341556f6ec
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:34:41 +0530

    Create 1617-StoneGameIv

---
+++ 1617-StoneGameIv/1617-StoneGameIv.py
@@ -0,0 +1,13 @@
+# Last updated: 15/9/2026, 11:34:45 pm
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        @cache
+        def dfs(i):
+            if i == 0: return False
+
+            for j in range(1, isqrt(i) + 1):
+                if not dfs(i - j ** 2): return True
+
+            return False
+
+        return dfs(n)
