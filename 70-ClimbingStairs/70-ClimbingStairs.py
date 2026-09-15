6d2a64ac029b6256a1eb7575a2222e844398d03f
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:37:06 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -0,0 +1,9 @@
+# Last updated: 15/9/2026, 11:37:10 pm
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        a, b = 1, 2
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+        return b
