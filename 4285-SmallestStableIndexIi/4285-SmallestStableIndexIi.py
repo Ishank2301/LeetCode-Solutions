5bb81454d54ff536720767bc4e14719beaeb69c2
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:32:35 +0530

    Create 4285-SmallestStableIndexIi

---
+++ 4285-SmallestStableIndexIi/4285-SmallestStableIndexIi.py
@@ -0,0 +1,16 @@
+# Last updated: 15/9/2026, 11:32:39 pm
+class Solution:
+    def firstStableIndex(self, A: List[int], k: int) -> int:
+        msf = -1
+        cand = cm = 0
+
+        for i, x in enumerate(A):
+            msf = max(msf, x)
+
+            if i == cand:
+                cm = msf
+
+            if x < cm - k:
+                cand = i + 1
+
+        return cand if cand < len(A) else -1
