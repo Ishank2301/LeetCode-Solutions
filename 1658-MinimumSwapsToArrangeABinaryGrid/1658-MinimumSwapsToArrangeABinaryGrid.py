06ebc09f807c130ababf1c0c1e5653c44a410ed6
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:34:39 +0530

    Create 1658-MinimumSwapsToArrangeABinaryGrid

---
+++ 1658-MinimumSwapsToArrangeABinaryGrid/1658-MinimumSwapsToArrangeABinaryGrid.py
@@ -0,0 +1,17 @@
+# Last updated: 15/9/2026, 11:34:43 pm
+class Solution:
+    def minSwaps(self, grid: List[List[int]]) -> int:
+
+        n, ans = len(grid), 0
+        right1 = lambda x: x[: : -1].index(1) if 1 in x else n
+        zeros = list(map(right1, grid))
+         
+        for i in range(n):
+            for j in range(i, n):
+                if zeros[j] + 1 < n - i: continue
+                zeros[i: j + 1] = [zeros[j]] + zeros[i: j]
+                ans+= j - i 
+                break
+            else: return -1
+
+        return ans       
