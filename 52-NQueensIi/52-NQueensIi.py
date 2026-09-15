5d9739c3abae23a8d70da1e1ad5f6b025f3326fc
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:37:23 +0530

    Create 52-NQueensIi

---
+++ 52-NQueensIi/52-NQueensIi.py
@@ -0,0 +1,36 @@
+# Last updated: 15/9/2026, 11:37:27 pm
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ld = [False] * (2 * n - 1)
+        rd = [False] * (2 * n - 1)
+        col = [False] * n
+
+        ans = 0
+
+        def placeQueen(row):
+            nonlocal ans
+
+            if row == n:
+                ans += 1
+                return
+
+            for j in range(n):
+                leftDiagonal = n - 1 + row - j
+                rightDiagonal = row + j
+
+                if col[j] or ld[leftDiagonal] or rd[rightDiagonal]:
+                    continue
+
+                col[j] = True
+                ld[leftDiagonal] = True
+                rd[rightDiagonal] = True
+
+                placeQueen(row + 1)
+
+                col[j] = False
+                ld[leftDiagonal] = False
+                rd[rightDiagonal] = False
+
+        placeQueen(0)
+
+        return ans
