527ea60ad15e59bdb02f004f623ca17e26ae49e6
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:34:57 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.py
@@ -0,0 +1,11 @@
+# Last updated: 15/9/2026, 11:35:00 pm
+from typing import List
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {}
+
+        for i, num in enumerate(sorted(set(arr)), 1):
+            rank[num] = i
+
+        return [rank[num] for num in arr]
