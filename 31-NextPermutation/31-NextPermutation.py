3aadf86376902ffa8af6ecc03601a49de8d9b832
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:37:58 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.py
@@ -0,0 +1,24 @@
+# Last updated: 15/9/2026, 11:38:02 pm
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+
+        # Step 1: Find pivot
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # Step 2: Find just larger element and swap
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Step 3: Reverse suffix
+        left = i + 1
+        right = n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]  # FIXED
+            left += 1
+            right -= 1
