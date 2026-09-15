4d3efd1391f531b2c50a1888d08bbcb6c232cf3e
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:33:16 +0530

    Create 3838-PathExistenceQueriesInAGraphI

---
+++ 3838-PathExistenceQueriesInAGraphI/3838-PathExistenceQueriesInAGraphI.py
@@ -0,0 +1,5 @@
+# Last updated: 15/9/2026, 11:33:20 pm
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], D: int, q: List[List[int]]) -> List[bool]:
+        C=[i:=0]+[i:=i+(prev+D<x) for prev, x in pairwise(nums)]
+        return [C[x]==C[y]for x, y in q]
