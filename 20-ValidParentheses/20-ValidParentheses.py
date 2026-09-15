75f531f911b189ee6e517d03746df13febb4a6f6
Ishank <ishankmishra579@gmail.com>
2026-09-15 23:38:13 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,38 @@
+# Last updated: 15/9/2026, 11:38:17 pm
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        matching = {')': '(', ']': '[', '}': '{'}
+        n = len(s)
+
+        if n <= 1:
+            return False
+
+        if s[0] == ')' or s[0] == ']' or s[0] == '}':
+            return False
+
+
+        for ch in s:
+            if ch in '([{':          # opening bracket
+                stack.append(ch)
+            else:                     # closing bracket
+                if not stack or stack[-1] != matching[ch]:
+                    return False
+                stack.pop()
+        
+        return len(stack) == 0        # stack must be empty at the end
+
+        #Is this exceptionally the best so far answer:
+
+
+
+
+
+
+
+
+
+
+
+
+
